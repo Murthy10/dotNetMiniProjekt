2d146a4714baacb23d86f90df5d94a85159cb1e4
Severin Bühler <sevi_buehler@hotmail.com>
2015-11-18 09:19:34 +0100

    added AutoService

--- AutoReservation.Service.Wcf/AutoReservationService.cs
+++ AutoReservation.Service.Wcf/AutoReservationService.cs
@@ -1,14 +1,109 @@
-﻿using System;
+﻿using AutoReservation.Common.Interfaces;
+using System;
 using System.Diagnostics;
+using AutoReservation.Common.DataTransferObjects;
+using System.Collections.Generic;
+using AutoReservation.BusinessLayer;
+
 
 namespace AutoReservation.Service.Wcf
 {
-    public class AutoReservationService
+    public class AutoReservationService: IAutoReservationService
     {
-
+        private AutoReservationBusinessComponent db = new AutoReservationBusinessComponent();
         private static void WriteActualMethod()
         {
             Console.WriteLine("Calling: " + new StackTrace().GetFrame(1).GetMethod().Name);
+        }
+
+        public void createAuto(AutoDto auto)
+        {
+            WriteActualMethod();
+            throw new NotImplementedException();
+        }
+
+        public void createKunde(KundeDto kunde)
+        {
+            WriteActualMethod();
+            throw new NotImplementedException();
+        }
+
+        public void createReservation(ReservationDto reservation)
+        {
+            WriteActualMethod();
+            throw new NotImplementedException();
+        }
+
+        public void deleteAuto(AutoDto auto)
+        {
+            WriteActualMethod();
+            throw new NotImplementedException();
+        }
+
+        public void deleteKunde(KundeDto kunde)
+        {
+            WriteActualMethod();
+            throw new NotImplementedException();
+        }
+
+        public void deleteReservation(ReservationDto reservation)
+        {
+            WriteActualMethod();
+            throw new NotImplementedException();
+        }
+
+        public List<AutoDto> getAllAutos()
+        {
+            WriteActualMethod();
+            throw new NotImplementedException();
+        }
+
+        public List<KundeDto> getAllKunden()
+        {
+            WriteActualMethod();
+            throw new NotImplementedException();
+        }
+
+        public List<ReservationDto> getAllReservationen()
+        {
+            WriteActualMethod();
+            throw new NotImplementedException();
+        }
+
+        public AutoDto getAuto(int id)
+        {
+            WriteActualMethod();
+            throw new NotImplementedException();
+        }
+
+        public KundeDto getKunde(int id)
+        {
+            WriteActualMethod();
+            throw new NotImplementedException();
+        }
+
+        public ReservationDto getReservation(int id)
+        {
+            WriteActualMethod();
+            throw new NotImplementedException();
+        }
+
+        public void updateAuto(AutoDto original, AutoDto modified)
+        {
+            WriteActualMethod();
+            throw new NotImplementedException();
+        }
+
+        public void updateKunde(KundeDto original, KundeDto modified)
+        {
+            WriteActualMethod();
+            throw new NotImplementedException();
+        }
+
+        public void updateReservation(ReservationDto original, ReservationDto modified)
+        {
+            WriteActualMethod();
+            throw new NotImplementedException();
         }
     }
 }
